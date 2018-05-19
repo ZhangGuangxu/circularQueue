ea21f7e04dbf9799ac8f31955aff0b407c4188fe
ZhangGuangxu <zhangxxxxxxx9623@126.com>
2018-05-19 10:31:15 +0800

    add source files

---
+++ circularqueue.go
@@ -0,0 +1,121 @@
+package circularqueue
+
+import "errors"
+
+const (
+	initSize = 32
+)
+
+// circularQueue allocate new memory when necessary.
+type circularQueue struct {
+	buffer []interface{}
+	readableIndex int
+	writableIndex int
+}
+
+// NewCircularQueue creates a circularQueue.
+// Usually, you use this function.
+func NewCircularQueue() *circularQueue {
+	return NewCircularQueueWithSize(initSize)
+}
+
+// NewCircularQueueWithSize creates a circularQueue with 
+// a size you specified.
+func NewCircularQueueWithSize(s int) *circularQueue {
+	return &circularQueue{
+		buffer: make([]interface{}, s),
+		readableIndex: 0,
+		writableIndex: 0,
+	}
+}
+
+func (b *circularQueue) Len() int {
+	if b.IsEmpty() {
+		return 0
+	}
+
+	var length int
+	if b.readableIndex < b.writableIndex {
+		length = b.writableIndex - b.readableIndex
+	} else if b.readableIndex > b.writableIndex {
+		length = len(b.buffer)-b.readableIndex + b.writableIndex
+	}
+	return length
+}
+
+// Push pushes a item into this queue.
+// Donot worry if this queue is full.
+func (b *circularQueue) Push(m interface{}) {
+	b.ensureWritableSpace()
+	b.buffer[b.writableIndex] = m
+	b.hasWritten()
+}
+
+func (b *circularQueue) ensureWritableSpace() {
+	if b.isFull() {
+		b.makeSpace()
+	}
+}
+
+func (b *circularQueue) makeSpace() {
+	buf := make([]interface{}, 1+cap(b.buffer)*2)
+	if b.readableIndex < b.writableIndex {
+		length := b.writableIndex - b.readableIndex
+		copy(buf, b.buffer[b.readableIndex:b.writableIndex])
+		b.readableIndex = 0
+		b.writableIndex = length
+	} else if b.readableIndex > b.writableIndex {
+		length := len(b.buffer)-b.readableIndex + b.writableIndex
+		copy(buf, b.buffer[b.readableIndex:len(b.buffer)])
+		copy(buf[len(b.buffer)-b.readableIndex:], b.buffer[:b.writableIndex])
+		b.readableIndex = 0
+		b.writableIndex = length
+	} else {
+		b.readableIndex = 0
+		b.writableIndex = 0
+	}
+	b.buffer = buf
+}
+
+func (b *circularQueue) hasWritten() {
+	b.writableIndex++
+	if b.writableIndex >= len(b.buffer) {
+		if b.readableIndex > 0 {
+			b.writableIndex = 0
+		}
+	}
+}
+
+// IsEmpty returns true if this queue if empty.
+func (b *circularQueue) IsEmpty() bool {
+	return b.readableIndex == b.writableIndex
+}
+
+func (b *circularQueue) isFull() bool {
+	return (b.readableIndex == 0 && b.writableIndex == len(b.buffer)) || 
+			b.writableIndex+1 == b.readableIndex
+}
+
+func (b *circularQueue) peek() interface{} {
+	return b.buffer[b.readableIndex]
+}
+
+func (b *circularQueue) retrieve() {
+	b.readableIndex++
+	if b.writableIndex >= len(b.buffer) {
+		b.writableIndex = 0
+	}
+	if b.readableIndex >= len(b.buffer) {
+		b.readableIndex = 0
+	}
+}
+
+// Pop pops a item.
+func (b *circularQueue) Pop() (interface{}, error) {
+	if b.IsEmpty() {
+		return nil, errors.New("circularQueue is empty")
+	}
+	m := b.peek()
+	b.retrieve()
+	return m, nil
+}
---
+++ circularqueue_test.go
@@ -0,0 +1,137 @@
+package circularqueue
+
+import (
+	"testing"
+)
+
+type message struct {
+	msgID int16
+	msg interface{}
+}
+
+type C2SLogin struct {
+	UID   int64 `json:"uid"`
+	Token string `json:"token"`
+}
+
+func TestPush(t *testing.T) {
+	q := NewCircularQueueWithSize(2)
+	if !q.IsEmpty() {
+		t.Errorf("q.IsEmpty() = %v, want %v", q.IsEmpty(), true)
+	}
+
+	m := &message{1, &C2SLogin{101, "sss"}}
+	q.Push(m)
+	if q.readableIndex != 0 {
+		t.Error("q.readleIndex is wrong")
+	}
+	if q.writableIndex != 1 {
+		t.Error("q.writableIndex is wrong")
+	}
+	if q.buffer[0] != m {
+		t.Error("q.buffer[0] is wrong")
+	}
+
+	{
+		m := &message{1, &C2SLogin{101, "sss"}}
+		q.Push(m)
+		if q.readableIndex != 0 {
+			t.Error("q.readleIndex is wrong")
+		}
+		if q.writableIndex != 2 {
+			t.Error("q.writableIndex is wrong")
+		}
+		if q.buffer[1] != m {
+			t.Error("q.buffer[0] is wrong")
+		}
+		if !q.isFull() {
+			t.Errorf("q.isFull() = %v, want %v", q.isFull(), true)
+		}
+	}
+
+	{
+		m := &message{1, &C2SLogin{101, "sss"}}
+		q.Push(m)
+		if q.readableIndex != 0 {
+			t.Error("q.readleIndex is wrong")
+		}
+		if q.writableIndex != 3 {
+			t.Error("q.writableIndex is wrong")
+		}
+		if q.buffer[2] != m {
+			t.Error("q.buffer[0] is wrong")
+		}
+		if q.isFull() {
+			t.Errorf("q.isFull() = %v, want %v", q.isFull(), false)
+		}
+		if len(q.buffer) != 5 {
+			t.Errorf("len(q.buffer) = %v, want %v", len(q.buffer), 5)
+		}
+		if cap(q.buffer) != 5 {
+			t.Errorf("cap(q.buffer) = %v, want %v", cap(q.buffer), 5)
+		}
+		if q.Len() != 3 {
+			t.Errorf("q.Len() = %v, want %v", q.Len(), 3)
+		}
+	}
+}
+
+func TestPop(t *testing.T) {
+	q := NewCircularQueueWithSize(2)
+
+	for i := 0; i < 2; i++ {
+		m := &message{1, &C2SLogin{101, "sss"}}
+		q.Push(m)
+	}
+
+	m, err := q.Pop()
+	if err != nil {
+		t.Error("q.Pop() return err")
+	}
+	v, ok := m.(*message)
+	if !ok {
+		t.Error("m.(Type) is not *message")
+	}
+	if v.msgID != 1 {
+		t.Errorf("m.msgID = %d, want %d", v.msgID, 1)
+	}
+
+	_, err = q.Pop()
+	if err != nil {
+		t.Error("q.Pop() return err")
+	}
+	if !q.IsEmpty() {
+		t.Errorf("q.IsEmpty() = %v, want %v", q.IsEmpty(), true)
+	}
+	_, err = q.Pop()
+	if err == nil {
+		t.Error("q.Pop() should return err")
+	}
+	
+	for i := 0; i < 2; i++ {
+		m := &message{1, &C2SLogin{101, "sss"}}
+		q.Push(m)
+	}
+
+	q.Pop()
+	if q.readableIndex != 1 {
+		t.Error("q.readableIndex != 1")
+	}
+	if q.writableIndex != 0 {
+		t.Error("q.writableIndex != 0")
+	}
+	if !q.isFull() {
+		t.Errorf("q.isFull() = %v, want %v", q.isFull(), true)
+	}
+
+	q.Pop()
+	if !q.IsEmpty() {
+		t.Errorf("q.IsEmpty() = %v, want %v", q.IsEmpty(), true)
+	}
+	if q.readableIndex != 0 {
+		t.Error("q.readableIndex != 0")
+	}
+	if q.writableIndex != 0 {
+		t.Error("q.writableIndex != 0")
+	}
+}
